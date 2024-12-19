0fa13661927a6a8dbbc342b2323c569c79808290
luv2s <ox_m@list.ru>
2024-12-19 11:53:12 +0200

    1

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,6 @@
 import {configureStore} from "@reduxjs/toolkit";
-import quote from "../features/quote/quoteSlice.ts"
-import balance from "../features/account/balanceSlice.ts"
+import balance from "../features/account/balanceSlice.ts";
+import quote from "../features/quote/quoteSlice.ts";
 
 export const store = configureStore({
     reducer: {
@@ -8,5 +8,6 @@ export const store = configureStore({
     }
 })
 
+// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
--- src/features/api/qouteAction.ts
+++ src/features/api/qouteAction.ts
@@ -1,13 +1,22 @@
-import {putQuote} from "../quote/quoteSlice.ts";
-import {AppDispatch} from "../../app/store.ts";
+// import {putQuote} from "../quote/quoteSlice.ts";
+// import {AppDispatch} from "../../app/store.ts";
+import {createAsyncThunk} from "@reduxjs/toolkit";
 
-
-export const fetchQuote = () => {
-    return (dispatch: AppDispatch) => {
-        dispatch(putQuote('Pending...'));
-        fetch('https://api.gameofthronesquotes.xyz/v1/random')
-            .then(response => response.json())
-            .then(data => dispatch(putQuote(data.sentence)))
-            .catch(() => dispatch(putQuote('Failed to fetch quote data')));
+export const fetchQuote = createAsyncThunk(
+    'fetch/quote',
+    async() => {
+        const response = await fetch('https://api.gameofthronesquotes.xyz/v1/random');
+        const data = await response.json();
+        return data.sentence;
     }
-}
+)
+
+// export const fetchQuote = () => {
+//     return (dispatch: AppDispatch) => {
+//         dispatch(putQuote('Pending...'));
+//         fetch('https://api.gameofthronesquotes.xyz/v1/random')
+//             .then(response => response.json())
+//             .then(data => dispatch(putQuote(data.sentence)))
+//             .catch(() => dispatch(putQuote('Failed to fetch quote data')));
+//     }
+// }
